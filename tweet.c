7b451ba574eb31140c418e6124805df005f870a6
Martin Häger <martin.haeger@gmail.com>
2009-04-01 11:31:29 +0200

    Further improvement of code quality

--- tweet.c
+++ tweet.c
@@ -25,7 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <string.h>
 #include <curl/curl.h>
 
-#define VERSION "20090331"
+#define VERSION "20090401"
+#define CONFIG_FILENAME ".cltwitter"
 #define MAX_MESSAGE_LENGTH 140
 #define MAX_USERNAME_PWD_LENGTH 100
 #define TWITTER_UPDATE_URL "http://twitter.com/statuses/update.xml"
@@ -33,8 +34,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define S(X) STRINGIFY(X)
 #define STRINGIFY(X) #X
 
-#define true 1
-#define false 0
+#define COMPLAIN_AND_EXIT(FORMAT, ...) { fprintf(stderr, FORMAT, ##__VA_ARGS__); exit(-1); }
+
+#define TRUE 1
+#define FALSE 0
 typedef unsigned char bool;
 
 typedef struct { 
@@ -44,23 +47,22 @@ typedef struct {
 
 config *parse_config(void);
 char *url_encode(char*);
-void complain_and_exit(char*);
 
 int main(int argc, char *argv[]) {
   
   /* require one argument */
   if (argc != 2)
-    complain_and_exit("cltwitter, version " VERSION "\nUsage: tweet [message]\nNOTE: Make sure that [message] is surrounded by quotes!\n");
+    COMPLAIN_AND_EXIT("cltwitter, version " VERSION "\nUsage: tweet [message]\nNOTE: Make sure that [message] is surrounded by quotes!\n");
   
   /* check message length */
   int length = strlen(argv[1]);
-  if (length > MAX_MESSAGE_LENGTH)
-    complain_and_exit("Error: Message can be at most " S(MAX_MESSAGE_LENGTH) " characters long according to Twitter.\n");
+  if (length == 0 || length > MAX_MESSAGE_LENGTH)
+    COMPLAIN_AND_EXIT("Error: Message must be between 1 and " S(MAX_MESSAGE_LENGTH) " characters long.\n");
   
   /* format POST data */
   int size = length + 8; // "status=" + '\0'
   char data[size];
-  char* url_encoded_status = url_encode(argv[1]);
+  char *url_encoded_status = url_encode(argv[1]);
   snprintf(data, size, "%s%s", "status=", url_encoded_status);
   free(url_encoded_status);
   
@@ -68,20 +70,18 @@ int main(int argc, char *argv[]) {
   config *cfg = parse_config();
   
   if(!cfg)
-    complain_and_exit("Error: Could not load configuration. Make sure that the " \
+    COMPLAIN_AND_EXIT("Error: Could not load configuration. Make sure that the " \
       "configuration file exists, is readable and contains the necessary " \
       "information (see the README for more information).\n");
   
-  size = 2*MAX_USERNAME_PWD_LENGTH + 2;
+  size = 2*MAX_USERNAME_PWD_LENGTH + 2; // ':' + '\0'
   char userpwd[size];
   snprintf(userpwd, size, "%s:%s", cfg->username, cfg->password);
   free(cfg);
   
   /* send update */
-  CURL *curl;
+  CURL *curl = curl_easy_init();
   CURLcode res;
-  
-  curl = curl_easy_init();
   
   if (curl) {
     
@@ -97,11 +97,12 @@ int main(int argc, char *argv[]) {
     
     res = curl_easy_perform(curl);
     
-    if (res != CURLE_OK)
-      printf("error: %s\n", curl_easy_strerror(res));
-    
     curl_easy_cleanup(curl);
     fclose(dev_null);
+    
+    if (res != CURLE_OK)
+      COMPLAIN_AND_EXIT("Error: Twitter connection gave the following error \"%s\n\"", 
+        curl_easy_strerror(res));
   }
   
   return 0;
@@ -109,46 +110,48 @@ int main(int argc, char *argv[]) {
 
 config *parse_config() {
   config *cfg;
-  FILE *fp;
-  static char cfg_file[] = ".cltwitter";
-  char* cfg_dir = getenv("HOME");
-  size_t size = strlen(cfg_dir) + strlen(cfg_file) + 2; // '/' + '\0'
-  char *cfg_path = (char*)malloc(size);
+  FILE *fp; 
+  char *cfg_dir = getenv("HOME");
+  size_t size = strlen(cfg_dir) + strlen(CONFIG_FILENAME) + 2; // '/' + '\0'
+  char *cfg_path = malloc(size);
   
   if (cfg_dir) {
-    snprintf(cfg_path, size, "%s/%s", cfg_dir, cfg_file);
+    snprintf(cfg_path, size, "%s/%s", cfg_dir, CONFIG_FILENAME);
   } else {
+    free(cfg_path);
     return NULL;
   }
   
-  if ( (fp = fopen(cfg_path, "r")) &&
-       (cfg = (config*)malloc(sizeof(config))) ) {
-    
+  fp = fopen(cfg_path, "r");
+  cfg = malloc(sizeof(config));
+  
+  if (fp && cfg) {    
     char line[1024];
     bool has_username, has_password;
-    has_username = has_password = false;
+    has_username = has_password = FALSE;
     while (fgets(line, sizeof line, fp) != NULL) {
       if (sscanf(line, "username=%" S(MAX_USERNAME_PWD_LENGTH) "s", cfg->username))
-        has_username = true;
+        has_username = TRUE;
       if(sscanf(line, "password=%" S(MAX_USERNAME_PWD_LENGTH) "s", cfg->password))
-        has_password = true;
+        has_password = TRUE;
     }
-    
-    if (!(has_username && has_password))
-      complain_and_exit("Error: Couldn't parse the configuration file. Please refer to the README file.\n");
     
     free(cfg_path);
     fclose(fp);
+    
+    if (!(has_username && has_password))
+      COMPLAIN_AND_EXIT("Error: Couldn't parse the configuration file. Please refer to the README file.\n");
+    
     return cfg;
   } else {
+    if (fp)
+      fclose(fp);
+    if (cfg)
+      free(cfg);
+      
     free(cfg_path);
     return NULL;
   }
-}
-
-void complain_and_exit(char* msg) {
-  fprintf(stderr, "%s", msg);
-  exit(-1);
 }
 
 char from_hex(char ch) {
