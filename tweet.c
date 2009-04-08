89a9866196133885b73175ac13c3b601db8d63f0
Martin Häger <martin.haeger@gmail.com>
2009-04-08 13:16:33 +0200

    Added GPL license headers to every source file

--- tweet.c
+++ tweet.c
@@ -1,21 +1,21 @@
 /*
 
-cltwitter - a command-line utility for Twitter
+This file is part of cltwitter - a command-line utility for Twitter
 
-Copyright 2008 Martin Häger <martin.haeger@gmail.com>
+Copyright 2008, 2009 Martin Häger <martin.haeger@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
+cltwitter is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+cltwitter is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with cltwitter.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
--- xml_helpers.c
+++ xml_helpers.c
@@ -1,3 +1,24 @@
+/*
+
+This file is part of cltwitter - a command-line utility for Twitter
+
+Copyright 2008, 2009 Martin Häger <martin.haeger@gmail.com>
+
+cltwitter is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+cltwitter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with cltwitter.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include "xml_helpers.h"
 
 xmlXPathObjectPtr eval_xpath(xmlDocPtr doc, xmlChar* xpath) {
