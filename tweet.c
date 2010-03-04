a329a9ff53910bdd3785da4d8ec2e04a1d38a72c
Martin Häger <martin.haeger@gmail.com>
2010-03-04 22:26:50 +0100

    Updated copyright info in file headers

--- tweet.c
+++ tweet.c
@@ -2,7 +2,7 @@
 
 This file is part of cltwitter - a command-line utility for Twitter
 
-Copyright 2008, 2009 Martin Häger <martin.haeger@gmail.com>
+Copyright 2008-2010 Martin Häger <martin.haeger@gmail.com>
 
 cltwitter is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
